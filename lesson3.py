2857cb61b6cd56847783ef90cd478382519d4cbe
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-07-08 17:11:54 +0200

    Create lesson3.py

---
+++ lesson3.py
@@ -0,0 +1,50 @@
+value = ['this is note', 'this is notissimo', 'note', 'this is a huge long, insanely long note', 'well, anyways']
+while True:
+     value_temporary = input()
+     if value_temporary == 'add':
+        value_to_add = input('введіть нотатку')
+        value.append(value_to_add)
+     elif value_temporary == 'earliest':
+          print(value)
+     elif value_temporary == 'latest':
+          print(value.reverse())
+     elif value_temporary == 'longest':
+          print(value.reverse())
+     elif value_temporary == 'shortest':
+          print(value.reverse())
+     elif value_temporary == 'Exit':
+          print(value.reverse())
+
+values = ['this is note', 'this is notissimo', 'note', 'this is a huge long, insanely long note', 'well, anyways']
+for element in values:
+         print(len(element))
+
+
+values = ['this is note', 'this is notissimo', 'note', 'this is a huge long, insanely long note', 'well, anyways']
+values_numbers = []
+for element in values:
+    values_numbers.append(len(element))
+print(values_numbers)
+
+
+second_element = {'this is note', 'this is notissimo'}
+print(2 in element)
+
+
+l1 = [10, 20, 30]
+l2 = [20, 30, 10]
+a = set(l1)
+b = set(l2)
+if a == b:
+    print("Lists l1 and l2 are equal")
+else:
+    print("Lists l1 and l2 are not equal")
+
+
+    print(len(second_element))
+
+sorted_values = sorted(value, key = len)
+sorted_values = sorted(value, key = len, reverse = True)
+
+
+
