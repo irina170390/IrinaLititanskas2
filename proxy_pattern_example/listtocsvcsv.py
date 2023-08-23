8c39dcd608a763da4cc5703a775063b91fce7094
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-08-24 00:36:49 +0200

    lesson14

--- proxy_pattern_example/listtocsvcsv.py
+++ proxy_pattern_example/listtocsvcsv.py
@@ -11,11 +11,21 @@ def remove_row():
     with open(r"D:\listtocsv.csv", "r") as file:
        for line in file:
          print(line)
+    with open(r"D:\listtocsv.csv", "r", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+          print(row[0], " - ", row[1])
     with open(r"D:\listtocsv.csv", "r") as file:
        lines = file.readlines()
     del lines[1]
     with open(r"D:\listtocsv.csv", "w") as file:
         file.writelines(lines)
+
+
+
+
+
+
 
 
 
