cab454bb60ba25a09f624b70e518e60ec3f7d88a
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-08-24 00:00:12 +0200

    lesson14

--- proxy_pattern_example/listtocsvcsv.py
+++ proxy_pattern_example/listtocsvcsv.py
@@ -9,9 +9,14 @@ def add_row():
 
 def remove_row():
     with open(r"D:\listtocsv.csv", "r") as file:
+       for line in file:
+         print(line)
+    with open(r"D:\listtocsv.csv", "r") as file:
        lines = file.readlines()
     del lines[1]
     with open(r"D:\listtocsv.csv", "w") as file:
         file.writelines(lines)
+
+
 
 
--- proxy_pattern_example/test_1.py
+++ proxy_pattern_example/test_1.py
@@ -4,5 +4,11 @@ def test_add():
     add_row()
     with open('listtocsv.csv', 'r') as file:
         lines = file.readlines()
-        assert lines == "['Kate', 33, 'google', 'Paris']"
+        assert lines == ['"[\'Kate\', 33, \'google\', \'Paris\']"\n']
+
+def test_remove():
+    add_row()
+    with open('listtocsv.csv', 'r') as file:
+        lines = file.readlines()
+        assert lines == ['"[\'Kate\', 33, \'google\', \'Paris\']"\n']
 
