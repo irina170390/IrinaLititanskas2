a34ef503c88969730e47a68f3d7bee67acc44d3d
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-08-24 21:49:05 +0200

    lesson14

--- proxy_pattern_example/listtocsvcsv.py
+++ proxy_pattern_example/listtocsvcsv.py
@@ -8,25 +8,34 @@ def add_row():
 
 
 def remove_row():
-    with open(r"D:\listtocsv.csv", "r") as file:
+    with open("listtocsv.csv", "r") as file:
        for line in file:
          print(line)
-    with open(r"D:\listtocsv.csv", "r", newline="") as file:
+    with open("listtocsv.csv", "r", newline="") as file:
         reader = csv.reader(file)
         for row in reader:
           print(row[0], " - ", row[1])
-    with open(r"D:\listtocsv.csv", "r") as file:
+    with open("listtocsv.csv", "r") as file:
        lines = file.readlines()
     del lines[1]
-    with open(r"D:\listtocsv.csv", "w") as file:
+    with open("listtocsv.csv", "w") as file:
         file.writelines(lines)
+        
 
 
 
 
 
 
+def usunPsa(self, ImiePsa):
 
+    with open('schronisko.csv', 'rb') as input, open('schronisko.csv', 'wb') as output:
+        writer = csv.writer(output)
+        for row in csv.reader(input):
+                if row[0] == ImiePsa:
+                    writer.writerow(row)
+    with open(self.plik, 'r') as f:
+            print(f.read())
 
 
 
