a9f7f4f1089afa35e947c91d1255604bd8fd3507
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-08-24 21:26:18 +0200

    lesoon14

--- proxy_pattern_example/listtocsvcsv.py
+++ proxy_pattern_example/listtocsvcsv.py
@@ -2,23 +2,23 @@ import csv
 
 def add_row():
     kate = ['Kate', 33, 'google', 'Paris'],
-    with open('listtocsvcsv', 'w') as file:
+    with open('listtocsv.csv', 'w') as file:
        writer = csv.writer(file)
        writer.writerow(kate)
 
 
 def remove_row():
-    with open(r"D:\listtocsvcsv", "r") as file:
+    with open(r"D:\listtocsv.csv", "r") as file:
        for line in file:
          print(line)
-    with open(r"D:\listtocsvcsv", "r", newline="") as file:
+    with open(r"D:\listtocsv.csv", "r", newline="") as file:
         reader = csv.reader(file)
         for row in reader:
           print(row[0], " - ", row[1])
-    with open(r"D:\listtocsvcsv", "r") as file:
+    with open(r"D:\listtocsv.csv", "r") as file:
        lines = file.readlines()
     del lines[1]
-    with open(r"D:\listtocsvcsv", "w") as file:
+    with open(r"D:\listtocsv.csv", "w") as file:
         file.writelines(lines)
 
 
