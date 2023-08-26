a9445c36d635105597731ab57a64004edf48d860
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-08-26 13:56:25 +0200

    lesson13

--- employee_singleton/__init__.py
+++ employee_singleton/__init__.py
@@ -1,7 +1,1 @@
-def singleton(_class):
-    def inner(*args):
-        if not hasattr(_class, 'instance'):
-            setattr(_class, 'instance', _class(*args))
-        return getattr(_class, 'instance')
-    return inner
 
