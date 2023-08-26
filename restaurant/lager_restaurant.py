8b1884e2a62e4ea3aa63dfbd906da9e9cd6be16d
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-08-26 13:47:08 +0200

    lesson13

--- restaurant/lager_restaurant.py
+++ restaurant/lager_restaurant.py
@@ -1,4 +1,4 @@
-from restaurant import RisottoRestaurant
+from restaurant.restaurant import RisottoRestaurant
 from restaurant.hardened_risotto import HardenedRisotto
 
 
@@ -8,13 +8,17 @@ class LagerRestaurant(RisottoRestaurant):
     def __init__(self):
         self.name = 'Risotto'
         self.__positions = ['lager', 'hardened']
+        self.number_of_risotto = 0
+
 
     @property
     def positions(self):
         return self.__positions
 
-    def get_beer(self, name):
+    def get_risotto(self, name):
         if name == 'lager':
             return 'here`s your lager'
         elif name == 'hardened':
             return HardenedRisotto()
+
+
