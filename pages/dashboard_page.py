47e3f8bb37b30941966b09c76b4bd3b36da93512
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-09-11 20:22:16 +0200

    lesson18

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -9,17 +9,14 @@ class Dashboard(BasePage):
         self.popular_categories_bags = ('xpath', '//a[@class="menu__link"][@href="https://rozetka.pl/torby-plecaki-i-etui-na-laptopy-80036/c80036/"]')
 
     def go_to_bags_and_backpacks(self):
-        locator = ('xpath', '//a[@href="https://rozetka.pl/torby-plecaki-i-etui-na-laptopy-80036/c80036/"]')
-        self.click_on_element(DashboardLocators['go_to_bags_and_backpacks_lokator'])
+        self.click_on_element(self.locators.bags_and_backpacks)
         return CategoryPage(self._driver)
 
     def go_to_login_form(self):
-        locator = ('xpath', '//a[@title="Sklep internetowy Rozetka.pl"]')
         self.click_on_element(self.locator.login)
 
 
     def search_for_game(self, message):
-        locator = ('xpath', '//img[@alt="LEGO Friends Dom rodziny Andrei 802 elementy (41449)"]')
         self.send_keys_into_field(self.locator.search, message)
         self.wait_until_element_appears(self.locator.first_search_result)
         self.press_enter(self.locator.search)
