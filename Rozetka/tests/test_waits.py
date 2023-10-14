2e0f13c01ba94029abcd4ec28d9477fcd449964a
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-10-14 18:07:37 +0200

    lesson

--- Rozetka/tests/test_waits.py
+++ Rozetka/tests/test_waits.py
@@ -7,11 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_01():
     driver = Chrome()
-    driver.get('https://google.com')
-    search_field_locator = "//textarea[@type='search']"
+    driver.get('https://rozetka.com.ua/notebook-bags/c80036/')
+    search_field_locator = "//a[@href='https://rozetka.com.ua/rivacase_8037_black/p38858304/']"
     driver.implicitly_wait(10)
     element = driver.find_element(by='xpath', value=search_field_locator)
-    element.send_keys('how to use webdriver')
+    element.send_keys('Сумка для ноутбука RivaCase 8037 Black 15.6" (8037 (Black))')
     action = ActionChains(driver)
     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
     action.key_down(Keys.CONTROL).send_keys('x').key_up(Keys.CONTROL).perform()
