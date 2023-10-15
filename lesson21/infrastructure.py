b29364a66631c1c33ec25f930456b3f1be0bfcda
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-10-15 13:47:57 +0200

    lesson20

--- lesson21/infrastructure.py
+++ lesson21/infrastructure.py
@@ -1,7 +1,14 @@
 import requests
 import json
+from requests.auth import HTTPBasicAuth
+from requests.exceptions import HTTPError
 
 url = "https://api.restful-api.dev/objects"
+class Mac:
+    def __init__(self, object_id, name, data=''):
+        self.object_id = object_id
+        self.name=name
+        self.data = data
 
 def get_an_object(object_id):
     response = requests.get(f"{url}/{object_id}")
@@ -22,4 +29,21 @@ def update_an_object(obj_id, changed_dict):
 def delete_an_object(obj_id):
     response = requests.delete(f'{url}/{obj_id}')
     return response
+
+def go_to_saucedemo_auth():
+    response = requests.get('https://www.saucedemo.com/', auth=HTTPBasicAuth('stand!ard_user', 'secret_sauce'))
+    return response
+
+def go_to_reqres_auth():
+    data = {"email": "eve.holt@reqres.in","password": "cityslicka"}
+    return requests.post('https://reqres.in/api/login', json=data)
+
+def create_user(token):
+    headers = {"content-type": "application/json",
+               'token':token}
+    return requests.post('https://reqres.in/api/users', json={
+    "name": "morpheus",
+    "job": "leader"
+},headers=headers)
+
 
