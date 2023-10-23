3425c2742c4bf6484b87f6667605956b8d6a5fbe
irina170390 <137342249+irina170390@users.noreply.github.com>
2023-10-23 18:22:47 +0200

    lesson22

--- lesson22/session_handler.py
+++ lesson22/session_handler.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
 
-engine = create_engine("postgresql://postgres:d2z76ctb@localhost:5432/postgres")
+engine = create_engine("postgresql://postgres:postgres@localhost:9999/postgres")
 
 __session = sessionmaker(engine)
 session: Session = __session()
